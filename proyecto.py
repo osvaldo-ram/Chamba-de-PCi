38b12c718f7115e6f1a330f3c0e2790e18f2e2f4
Jesus Osvaldo Ramos Perez <a01713833@tec.mx>
2024-08-18 13:41:13 -0600

    Add files via upload

---
+++ proyecto.py
@@ -0,0 +1,28 @@
+'''
+Jesús Osvaldo Ramos Pérez A01713833
+1.Entrada:
+1.1 Solicitar la longitud de la contraseña.
+1.2 Preguntar si desean incluir letras mayúsculas.
+1.3 Preguntar si desean incluir letras minúsculas.
+1.4 Preguntar si desean incluir números.
+1.5 Preguntar si desean incluir símbolos.
+
+2.Inicialización del programa:
+2.1 Crear una lista vacía para almacenar los caracteres válidos.
+2.2 Crear una lista para almacenar la contraseña generada.
+
+3.Agregar los tipos de caracteres:
+3.1 Si se permite letras mayúsculas, agregar letras mayúsculas a la lista de caracteres válidos.
+3.2 Si se permite letras minúsculas, agregar letras minúsculas a la lista de caracteres válidos.
+3.3 Si se permiten números, agregar números a la lista de caracteres válidos.
+3.4 Si se permiten símbolos, agregar símbolos a la lista de caracteres válidos.
+
+4.Generar la contraseña:
+4.1 Para cada posición de la longitud deseada de la contraseña:
+4.2 Seleccionar un carácter aleatorio de la lista de caracteres válidos.
+4.3 Añadir el carácter seleccionado a la lista de la contraseña.
+
+5.Mostrar la contraseña:
+5.1 Convertir la lista de la contraseña a una cadena de texto.
+5.2 Mostrar la contraseña generada al usuario.
+'''

358984293a97f4c868f96d446a552fcbbb2a06fd
Jesus Osvaldo Ramos Perez <a01713833@tec.mx>
2024-10-13 16:15:49 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 #Jesús Osvaldo Ramos Pérez A01713833
 # se importa la librería random, para usar el método choice
+#Se hizo una matriz que contiene los tipos de datos para generar la contraseña
 import random
-lista1=list("aeiou")
-lista2=list("AEIOU")
-lista3=list("1234567890")
-lista4=list("-*+./_")
+datos=[
+list("aeiou"),
+list("AEIOU"),
+list("1234567890"),
+list("-*+./_")]
 lista5=[]
 seguridad=0
 longitud=int(input("Cuál es la longitud de caracteres que quieres en tu contraseña? : "))
 mayusculas=(input("Quieres agregar mayusculas a tu contraseña?si/no: "))
 #Los condicionales los utilice para delimitar lo que escoge o no el usuario
+#Se almacena el tipo de dato correspondiente a su poscion dentro de la matriz
 if mayusculas=='si':
-    lista5.extend(lista2)
+    lista5.extend(datos[1])
     seguridad+=2
 minusculas=(input("Quieres agregar minusculas a tu contraseña?si/no: "))
 if minusculas=='si':
-    lista5.extend(lista1)
+    lista5.extend(datos[0])
     seguridad+=1
 numeros=(input("Quieres agregar numeros a tu contraseña?si/no: "))
 if numeros=='si':
-    lista5.extend(lista3)
+    lista5.extend(datos[2])
     seguridad+=3
 simbolos=(input("Quieres agregar simbolos tu contraseña?si/no: "))
 if simbolos=='si':
-    lista5.extend(lista4)
+    lista5.extend(datos[3])
     seguridad+=4
 '''
 El for lo utilice para generar la contraseña basada en la longitud de lo que pidió el usuario.

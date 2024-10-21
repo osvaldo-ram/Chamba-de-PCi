6633d8eda78e61a2727592fc23b9a30a6f934103
Jesus Osvaldo Ramos Perez <a01713833@tec.mx>
2024-10-20 18:47:15 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -2,51 +2,94 @@
 # se importa la librería random, para usar el método choice
 #Se hizo una matriz que contiene los tipos de datos para generar la contraseña
 import random
-datos=[
-list("aeiou"),
-list("AEIOU"),
-list("1234567890"),
-list("-*+./_")]
-lista5=[]
-seguridad=0
-longitud=int(input("Cuál es la longitud de caracteres que quieres en tu contraseña? : "))
-mayusculas=(input("Quieres agregar mayusculas a tu contraseña?si/no: "))
-#Los condicionales los utilice para delimitar lo que escoge o no el usuario
-#Se almacena el tipo de dato correspondiente a su poscion dentro de la matriz
-if mayusculas=='si':
-    lista5.extend(datos[1])
-    seguridad+=2
-minusculas=(input("Quieres agregar minusculas a tu contraseña?si/no: "))
-if minusculas=='si':
-    lista5.extend(datos[0])
-    seguridad+=1
-numeros=(input("Quieres agregar numeros a tu contraseña?si/no: "))
-if numeros=='si':
-    lista5.extend(datos[2])
-    seguridad+=3
-simbolos=(input("Quieres agregar simbolos tu contraseña?si/no: "))
-if simbolos=='si':
-    lista5.extend(datos[3])
-    seguridad+=4
-'''
-El for lo utilice para generar la contraseña basada en la longitud de lo que pidió el usuario.
-Se usó el método choice para escoger los caracteres que tendrá la contraseña
-'''
-def generar_contraseña (long,lista):
-    listaF=''    
-    for i in range (long):
-        listaF+=random.choice(lista)
-    return listaF
 
-contraseña=generar_contraseña(longitud,lista5)
-if seguridad>7:
-    print("La contraseña es segura")
-if seguridad>4 and seguridad<7:
-    print("La contraseña es aceptable")
-if seguridad<4:
-    print("La contraseña no es segura")
+datos = [
+    list("aeiou"),
+    list("AEIOU"),
+    list("1234567890"),
+    list("-*+./_")
+]
+seguridad = 0
+
+
+def generar_contraseña(longitud, lista_tipos):
+    """
+    Genera una contraseña garantizando al menos un carácter de cada tipo seleccionado.
+    """
+    listaF = []
     
-print("la contraseña generada es: ",contraseña, " y tiene un numero de caracteres de ",len(contraseña))
+    # Garantizar al menos un carácter de cada tipo seleccionado
+    for tipo in lista_tipos:
+        listaF.append(random.choice(tipo))
+    
+    # Rellenar el resto de la contraseña al azar
+    while len(listaF) < longitud:
+        listaF.append(random.choice(random.choice(lista_tipos)))
+    
+    # Mezclar los caracteres para mayor aleatoriedad
+    #shuffle es un método que organiza el orden de la lista
+    #https://www.w3schools.com/python/ref_random_shuffle.asp
+    random.shuffle(listaF)
+    
+    #join convierte los elementos de una lista en un solo string, siendo separados por un caracter, en este caso usamos un caracter vacio para tener la contraseña correctamente generada
+    #https://www.w3schools.com/python/ref_string_join.asp
+    return ''.join(listaF)
 
 
+def evaluar_seguridad(seguridad):
+    """
+    Evalúa y muestra el nivel de seguridad de la contraseña.
+    """
+    if seguridad > 7:
+        print("La contraseña es segura")
+    elif 4 < seguridad <= 7:
+        print("La contraseña es aceptable")
+    else:
+        print("La contraseña no es segura")
 
+#Ciclo para generar contraseñas mientras el usuario lo requiera
+while True:
+    menu = int(input("1-Crear contraseña  2-Salir  "))
+    
+    if menu == 1:
+        longitud = int(input("¿Cuál es la longitud de caracteres que quieres en tu contraseña? (Min. 4 caracteres) : "))
+        #Ciclo para asegurar el minimo para generar la longitud de la contraseña
+        while longitud<4 :
+            longitud = int(input("¿Cuál es la longitud de caracteres que quieres en tu contraseña? (Min. 4 caracteres) : "))
+            
+            
+        lista_tipos = []
+        
+        mayusculas = input("¿Quieres agregar mayúsculas a tu contraseña? (si/no): ")
+        if mayusculas.lower() == 'si':
+            lista_tipos.append(datos[1])
+            seguridad += 2
+
+        minusculas = input("¿Quieres agregar minúsculas a tu contraseña? (si/no): ")
+        if minusculas.lower() == 'si':
+            lista_tipos.append(datos[0])
+            seguridad += 1
+
+        numeros = input("¿Quieres agregar números a tu contraseña? (si/no): ")
+        if numeros.lower() == 'si':
+            lista_tipos.append(datos[2])
+            seguridad += 3
+
+        simbolos = input("¿Quieres agregar símbolos a tu contraseña? (si/no): ")
+        if simbolos.lower() == 'si':
+            lista_tipos.append(datos[3])
+            seguridad += 4
+
+        if lista_tipos:
+            contraseña = generar_contraseña(longitud, lista_tipos)
+            evaluar_seguridad(seguridad)
+            
+            print(f"La contraseña generada es: {contraseña} y tiene un número de caracteres de {len(contraseña)}")
+        else:
+            print("Debes seleccionar al menos un tipo de carácter para generar la contraseña.")
+    
+    else:
+        break
+
+
+    
